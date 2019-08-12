7b015c6b51309d3d32392d30b88c6ce939528bca
Mishra <ayushmishraims@gmail.com>
2019-08-12 12:30:56 +0530

    adding service

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { map } from 'rxjs/operators';
+import {ProfileService} from '../../service/profile.service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-admin-orders',
@@ -6,10 +12,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-orders.component.css']
 })
 export class AdminOrdersComponent implements OnInit {
-
-  constructor() { }
-
+  public  genderList = [{name : 'Male'}, {name : 'Female'}];
+  uploadProgress : any;
+  ImagePath : string = '';
+  disableBtn = true;
+  optionValue : number;
+  constructor(
+    private afStorage : AngularFireStorage,
+    private profileServices : ProfileService,
+    private router : Router
+  ) { }
+  
   ngOnInit() {
+    
   }
+
+  selectGender(event: any):void{
+    console.log(event.target.value)
+  }
+
+  uploadProductImage(event) {
+  const UserId = localStorage.getItem('userId');
+  const profileImage = this.afStorage.upload(`/profileImage/${UserId}`, event.target.files[0]); 
+  profileImage.then((snapshot) =>{
+    this.ImagePath = snapshot.downloadURL;
+    this.disableBtn = false;
+})
+  
+}
+
+
+profileSubmit(profileForm): void {
+  const profileObjectCopy = {...profileForm.value}
+  const profileObject = {...profileObjectCopy, profilImage : this.ImagePath};
+  this.profileServices.saveProfileData(profileObject).then((data) => {
+      this.router.navigate(['/products'])
+  })
+}
 
 }
---
+++ src/app/service/profile.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(
+    private db: AngularFireDatabase
+  ) { }
+
+  saveProfileData(profileObject){
+    const userId = localStorage.getItem('userId')
+    return this.db.object('/users/' + userId).update(profileObject);
+    //this.db.list('/users').;
+  }
+}
